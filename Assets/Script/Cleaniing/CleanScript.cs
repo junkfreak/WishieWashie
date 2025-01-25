87d6f062b5134ae128bd80c90d43efc64415de3f
KatsoepXPLAB <86463969+KatsoepXPLAB@users.noreply.github.com>
2025-01-25 19:56:39 +0100

    hands player prefab

--- Assets/Script/Cleaniing/CleanScript.cs
+++ Assets/Script/Cleaniing/CleanScript.cs
@@ -19,7 +19,9 @@ public class CleanScript : MonoBehaviour
     public bool shoot;
     public PlayerInput pInput;
     public InputActionAsset pcntrl;
-    public float rayDist;
+    public float rayDist, handDist, rayRadius;
+
+    public GameObject handL, handR;
 
     public LayerMask mask;
     public void Awake()
@@ -36,7 +38,7 @@ public class CleanScript : MonoBehaviour
         _material = reus.GetComponent<SkinnedMeshRenderer>().material;
         dirtMaskTextureBase = _material.GetTexture("_greenmask") as Texture2D;
 
-        _dirtMaskBase = new Texture2D(dirtMaskTextureBase.width, dirtMaskTextureBase.height);
+        _dirtMaskBase = new Texture2D(dirtMaskTextureBase.width , dirtMaskTextureBase.height);
         //_dirtMaskBase.SetPixels(dirtMaskTextureBase.GetPixels());
         _dirtMaskBase.Apply();
         _material.SetTexture("_greenmask", _dirtMaskBase);
@@ -51,6 +53,79 @@ public class CleanScript : MonoBehaviour
 
     private void Update()
     {
+        Debug.DrawRay(handL.transform.position, - handL.transform.forward, Color.red, handDist);
+        if (Physics.Raycast(handL.transform.position, - handL.transform.forward, out RaycastHit hithand, handDist, ~mask))
+        {
+            Vector2 textureCoord = hithand.textureCoord;
+            Debug.Log(textureCoord);
+
+            int pixlX = (int)(textureCoord.x * _dirtMaskBase.width);
+            int pixlY = (int)(textureCoord.y * _dirtMaskBase.height);
+
+            Vector2Int paintPixlPos = new Vector2Int(pixlX, pixlY);
+
+            Debug.Log("UV" + textureCoord + " ; pixels: " + paintPixlPos);
+
+
+            int pixlOffsetX = pixlX - (_brush.width / 6);
+            int pixlOffsetY = pixlY - (_brush.height / 6);
+
+            for (int x = 0; x < _brush.width; x++)
+            {
+                for (int y = 0; y < _brush.height; y++)
+                {
+                    //Debug.Log("painting");
+                    Color pixDirt = _brush.GetPixel(x, y);
+                    Color pixMask = _dirtMaskBase.GetPixel(pixlOffsetX + x, pixlOffsetY + y);
+
+
+
+                    _dirtMaskBase.SetPixel(pixlOffsetX + x, pixlOffsetY + y, new Color(0, pixMask.g * pixDirt.g, 0));
+                }
+
+            }
+
+            _dirtMaskBase.Apply();
+        }
+
+
+        Debug.DrawRay(handR.transform.position, -handR.transform.forward, Color.red, handDist);
+       // if (Physics.SphereCast(handR.transform.position,rayRadius, -handR.transform.position, out RaycastHit hithand2, 0.5f, ~mask))
+          if (Physics.Raycast(handR.transform.position, -handR.transform.forward, out RaycastHit hithand2, handDist, ~mask))
+        {
+            Vector2 textureCoord = hithand2.textureCoord;
+            Debug.Log(textureCoord);
+
+            int pixlX = (int)(textureCoord.x * _dirtMaskBase.width);
+            int pixlY = (int)(textureCoord.y * _dirtMaskBase.height);
+
+            Vector2Int paintPixlPos = new Vector2Int(pixlX, pixlY);
+
+            Debug.Log("UV" + textureCoord + " ; pixels: " + paintPixlPos);
+
+
+            int pixlOffsetX = pixlX - (_brush.width / 6);
+            int pixlOffsetY = pixlY - (_brush.height / 6);
+
+            for (int x = 0; x < _brush.width; x++)
+            {
+                for (int y = 0; y < _brush.height; y++)
+                {
+                    //Debug.Log("painting");
+                    Color pixDirt = _brush.GetPixel(x, y);
+                    Color pixMask = _dirtMaskBase.GetPixel(pixlOffsetX + x, pixlOffsetY + y);
+
+
+
+                    _dirtMaskBase.SetPixel(pixlOffsetX + x, pixlOffsetY + y, new Color(0, pixMask.g * pixDirt.g, 0));
+                }
+
+            }
+
+            _dirtMaskBase.Apply();
+        }
+
+
         //Debug.DrawLine(_camera.transform.position, _camera.transform.forward, Color.red, rayDist);
         Debug.DrawRay(_camera.transform.position, _camera.transform.forward, Color.yellow, rayDist);
         if (shoot)
