b3f56bf3fc0b8332eaea6cf2e5b4b3085ed6a064
KatsoepXPLAB <86463969+KatsoepXPLAB@users.noreply.github.com>
2025-01-25 01:28:04 +0100

    movement

--- Assets/Script/Character controller/Movement.cs
+++ Assets/Script/Character controller/Movement.cs
@@ -25,15 +25,71 @@ public class Movement : MonoBehaviour
     public bool grounded, sprint;
 
     public float jumpHeight;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Awake()
     {
-        
+        moveAct = pcntrl.FindActionMap("Player").FindAction("Move");
+        rotateAct = pcntrl.FindActionMap("Player").FindAction("Look");
+        sprintAct = pcntrl.FindActionMap("Player").FindAction("Sprint");
+
+
+
+        moveAct.performed += context => moveInput = context.ReadValue<Vector2>();
+        moveAct.canceled += context => moveInput = Vector2.zero;
+
+        rotateAct.performed += context => rotateInput = context.ReadValue<Vector2>();
+        rotateAct.canceled += context => rotateInput = Vector2.zero;
+
+        sprintAct.performed += context => sprint = true;
+        sprintAct.canceled += context => sprint = false;
+
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Bewegen();
+    }
+
+    public void Bewegen()
+    {
+        Vector3 camF = cam.transform.forward;
+        Vector3 camR = cam.transform.right;
+
+        camF.y = 0;
+        camR.y = 0;
+        camF = camF.normalized;
+        camR = camR.normalized;
+
+        Vector3 forwardRelative = moveInput.y * camF;
+        Vector3 rightRelative = moveInput.x * camR;
+
+
+
+        Vector3 camRelativMove = Vector3.ClampMagnitude(forwardRelative + rightRelative, 1);
+        Vector3 moveDir = camRelativMove * speed;
+
+
+        contrlr.Move(moveDir * Time.deltaTime * speed);
+
+        if (grounded == false)
+        {
+            playerVelo += Physics.gravity / 20;
+
+            contrlr.Move(playerVelo * Time.deltaTime);
+        }
+
+    }
+
+    public void Draaien()
+    {
+        if (rotateInput.sqrMagnitude > 0.1f)
+        {
+            float targRot = Mathf.Atan2(-rotateInput.y, rotateInput.x) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+            Quaternion lookAt = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, targRot, 0), rotSpeed);
+            this.transform.rotation = lookAt;
+        }
     }
 }
