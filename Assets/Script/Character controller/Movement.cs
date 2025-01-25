6369d07803167fe1b5b021c0907eafec5da24c7e
KatsoepXPLAB <86463969+KatsoepXPLAB@users.noreply.github.com>
2025-01-25 01:15:38 +0100

    movescript

--- Assets/Script/Character controller/Movement.cs
+++ Assets/Script/Character controller/Movement.cs
@@ -1,7 +1,30 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    public PlayerInput pInput;
+    public InputActionAsset pcntrl;
+
+    private InputAction moveAct;
+    private InputAction rotateAct;
+    private InputAction sprintAct;
+    private InputAction jumpAct;
+
+    private Vector2 moveInput;
+    private Vector2 rotateInput;
+
+    public float speed, rotSpeed, sprintSpeed, normSpeed;
+    public Camera cam;
+
+    public CharacterController contrlr;
+    public Vector3 playerVelo;
+    public float gravity;
+    public bool grounded, sprint;
+
+    public float jumpHeight;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
